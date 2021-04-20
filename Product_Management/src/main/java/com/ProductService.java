402be316d25f557a4f4608b89ac8e8009c717e9b
Isuri Nawanjanee <isujayawardana1220@gmail.com>
2021-04-21 00:01:13 +0530

    Completed the crud on Product Management

---
+++ Product_Management/src/main/java/com/ProductService.java
@@ -0,0 +1,25 @@
+package com;
+
+import model.Product;
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document; 
+
+@Path("/Products")
+public class ProductService {
+
+	Product productObj = new Product();
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readProducts()
+	 {
+	 return productObj.readProducts();
+	 }
+}
